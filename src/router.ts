29e4e2ae1ad67690861432cd71130a108b1a3f25
jhordy <jhooordy@gmail.com>
2023-10-20 19:02:16 -0300

    chore: create endpoint to get events

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,25 @@
 import { Router } from 'express';
+import { validate, validateResponse } from './middlewares';
+import { EventFilterQuerySchema, EventsSchema } from './validators/events';
+import { getEvents } from './services/getEvents';
+import { Status } from '@prisma/client';
 
 export const router = Router();
 
-router.get('/', (req, res) => {
-  console.log('Hello World');
-});
+router.get(
+  '/events',
+  validate({
+    query: EventFilterQuerySchema,
+  }),
+  async (req, res) => {
+    const events = validateResponse({
+      data: await getEvents({
+        status: req.query.status ?? Status.LIVE,
+        date: req.query.date,
+      }),
+      schema: EventsSchema,
+    });
+
+    res.json(events).status(200);
+  }
+);
