55e6cdb00791f03c87674d092d322b2996b2a788
jhordy <jhooordy@gmail.com>
2023-10-20 22:54:24 -0300

    chore: add cors and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 require('express-async-errors');
 import express from 'express';
 import { router } from './router';
+import { serve, setup } from 'swagger-ui-express';
+import cors from 'cors';
 
 export const app = express();
+app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
+app.use('/api-docs', serve, setup(require('../swagger.json')));
